e6e0a4d73113f7bbbe7a607bc9b5f726993e2764
NiclasvanEyk <niclas.eyk@gmail.com>
2016-04-23 19:32:25 +0200

    Kommentiert und formatiert

--- rpc_checkIP_client.c
+++ rpc_checkIP_client.c
@@ -1,32 +1,41 @@
 #include "rpc_checkIP.h"
 
+/**
+ * Hier wird die konkrete Methode check_ip_1 implementiert
+ * //TODO: ist glaub ich gar nicht nötig, es ist eher gefordert, die ip-adresse
+ * 				 an den Server zu übermitteln und dessen Antwort zu verarbeiten
+ * @param host ?
+ * @param ip   ?
+ */
 void check_ip_1(char *host, ip_str *ip) {
 	CLIENT *clnt;
 	int  *result_1;
-	ip_str  checkip_1_arg = *ip;
-  printf("%s\n", checkip_1_arg);
+	ip_str  ip_adress = *ip;
+  printf("%s\n", ip_adress);
 
-#ifndef	DEBUG
+	#ifndef	DEBUG
 	clnt = clnt_create (host, CHECK_IP, CHECK_IP_1, "udp");
 	if (clnt == NULL) {
 		clnt_pcreateerror (host);
 		exit (1);
 	}
-#endif	/* DEBUG */
+	#endif	/* DEBUG */
 
-	result_1 = checkip_1(&checkip_1_arg, clnt);
+	result_1 = checkip_1(&ip_adress, clnt);
 	if (result_1 == (int *) NULL) {
 		clnt_perror (clnt, "call failed");
 	}
-#ifndef	DEBUG
+	#ifndef	DEBUG
 	clnt_destroy (clnt);
-#endif	 /* DEBUG */
+	#endif	 /* DEBUG */
 }
 
-
+/**
+ * Main
+ */
 int main (int argc, char *argv[]) {
 	char *host;
-  ip_str  checkip_1_arg = argv[2];
+  ip_str ip_adress = argv[2];
 
 	if (argc < 3) {
 		printf ("usage: %s server_host\n", argv[0]);
@@ -34,7 +43,7 @@ int main (int argc, char *argv[]) {
 	}
 	host = argv[1];
   printf("host: %s\n", host);
-  printf("ip: %s\n", checkip_1_arg);
-	check_ip_1 (host, &checkip_1_arg);
+  printf("ip: %s\n", ip_adress);
+	check_ip_1 (host, &ip_adress);
 	exit (0);
 }
